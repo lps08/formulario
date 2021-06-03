7642a1af9bcc12424f8944845b9cf8ad73041744
lps08 <lucianolps08@gmail.com>
2021-06-03 20:42:30 -0300

    implemented database helper to connect to mysql

--- lib/controllers/helpers/database_helper.dart
+++ lib/controllers/helpers/database_helper.dart
@@ -1,2 +1,20 @@
 //TODO: implement database helper
-class DatabaseHelper {}
+import 'package:mysql1/mysql1.dart';
+
+class DatabaseHelper {
+  static final _host = '';
+  static final _port = 0;
+  static final _user = '';
+  static final _databaseName = '';
+  static final _passwd = '';
+
+  //connect to mysql database
+  static Future<MySqlConnection> connectDatabase() async {
+    return await MySqlConnection.connect(ConnectionSettings(
+        host: _host,
+        port: _port,
+        user: _user,
+        db: _databaseName,
+        password: _passwd));
+  }
+}
