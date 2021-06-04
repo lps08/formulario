349cd850fdb848e237aafc6fdf602b96fd08c5be
lps08 <lucianolps08@gmail.com>
2021-06-04 11:30:36 -0300

    init implement to dao class

--- lib/controllers/dao/dao.dart
+++ lib/controllers/dao/dao.dart
@@ -1,2 +1,20 @@
 //TODO: implement abastract class
-class DAO {}
+import 'package:formulario/controllers/helpers/database_helper.dart';
+import 'package:formulario/models/model.dart';
+import 'package:mysql1/mysql1.dart';
+
+abstract class DAO<T extends Model> {
+  late String table;
+
+  Future<void> insert(T) async {
+    // INSERT INTO Cliente (nome, sexo, nascimento, raca, telefone, endereco, bairro, municipio) VALUES ('Cliente 1', 'masculino', '2021-06-04', 'branca', 086999948302, 'Rua meclanche', 'cria', 'Piaui');
+    MySqlConnection connection = await DatabaseHelper.connectDatabase();
+  }
+
+  Future<void> delete(int codebars) async {}
+
+  Future<void> update(T) async {}
+
+  Future<List<T>> get(int codebars);
+  Future<List<T>> getAll();
+}
