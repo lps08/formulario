94a35a177bdec7645581a3ae88bbebdd51877354
lps08 <lucianolps08@gmail.com>
2021-06-05 12:04:21 -0300

    implement insert and delete method

--- lib/controllers/dao/dao.dart
+++ lib/controllers/dao/dao.dart
@@ -6,15 +6,45 @@ import 'package:mysql1/mysql1.dart';
 abstract class DAO<T extends Model> {
   late String table;
 
-  Future<void> insert(T) async {
+  Future<bool> insert(T) async {
     // INSERT INTO Cliente (nome, sexo, nascimento, raca, telefone, endereco, bairro, municipio) VALUES ('Cliente 1', 'masculino', '2021-06-04', 'branca', 086999948302, 'Rua meclanche', 'cria', 'Piaui');
     MySqlConnection connection = await DatabaseHelper.connectDatabase();
+    String query =
+        'INSERT INTO $table (${T.toMap().keys.join(',')}) VALUES (${T.toMap().values.join(',')});';
+
+    Results res = await connection.query(query);
+
+    if (res.insertId!.isNaN) return false;
+    return true;
   }
 
-  Future<void> delete(int codebars) async {}
+  Future<bool> delete(
+      {required String where, required dynamic whereArgs}) async {
+    MySqlConnection connection = await DatabaseHelper.connectDatabase();
+    String query = 'DELETE FROM $table WHERE $where = $whereArgs;';
+
+    Results res = await connection.query(query);
+
+    if (res.insertId!.isNaN) return false;
+    return true;
+  }
 
   Future<void> update(T) async {}
 
   Future<List<T>> get(int codebars);
   Future<List<T>> getAll();
+}
+
+main(List<String> args) {
+  Map<String, dynamic> teste = {
+    'nome': 'nome',
+    'numero': 10,
+  };
+
+  String where = 'cod';
+  dynamic whereArgs = '1';
+
+  String query = 'DELETE FROM tabela WHERE $where = $whereArgs;';
+
+  print(query);
 }
