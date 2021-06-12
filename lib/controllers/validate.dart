1691057acb67280b8e5b49e764a9aa5572d4ec03
lps08 <lucianolps08@gmail.com>
2021-06-12 09:27:37 -0300

    add validate class

---
+++ lib/controllers/validate.dart
@@ -0,0 +1,13 @@
+class Validade {
+  static String? onlyString(String nome) {
+    if (nome.contains(RegExp(r'[0-9]')))
+      return 'Valores numéricos não são permitidos';
+    return null;
+  }
+
+  static String? onlyNumber(String tele) {
+    if (tele.contains(RegExp(r'[A-Z]')))
+      return 'Neste campo só é permitido números';
+    return null;
+  }
+}
