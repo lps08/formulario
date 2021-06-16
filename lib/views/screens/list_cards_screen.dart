a07a501ea3b0eb341c2fef272030f68fa8c6fc05
lps08 <lucianolps08@gmail.com>
2021-06-16 10:46:34 -0300

    cards page

---
+++ lib/views/screens/list_cards_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//TODO: implment cards list
+
+class ListCardsPage extends StatefulWidget {
+  const ListCardsPage({Key? key}) : super(key: key);
+
+  @override
+  _ListCardsPageState createState() => _ListCardsPageState();
+}
+
+class _ListCardsPageState extends State<ListCardsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
