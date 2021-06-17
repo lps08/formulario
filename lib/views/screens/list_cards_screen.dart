ed063681c42b8b098f25c36659e8f0eb2044e458
lps08 <lucianolps08@gmail.com>
2021-06-16 21:59:14 -0300

    page list clients

--- lib/views/screens/list_cards_screen.dart
+++ lib/views/screens/list_cards_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:formulario/controllers/dao/client_dao.dart';
+import 'package:formulario/views/widgets/card_cliente.dart';
 
 //TODO: implment cards list
 
@@ -9,14 +11,29 @@ class ListCardsPage extends StatefulWidget {
   _ListCardsPageState createState() => _ListCardsPageState();
 }
 
+ClientDAO _clientDAO = ClientDAO();
+
+void _getWidgetsCards() async {
+  List<Widget> containers = [];
+
+  var listClients = await _clientDAO.getAll();
+  print(listClients.length());
+}
+
 class _ListCardsPageState extends State<ListCardsPage> {
   @override
   Widget build(BuildContext context) {
+    _getWidgetsCards();
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista clientes'),
       ),
-      body: Container(),
+      body: SafeArea(
+          child: Column(
+        children: [
+          CardCliente(),
+        ],
+      )),
     );
   }
 }
