633688500f8b08c0918ee3261d24420c3eff98c6
lps08 <lucianolps08@gmail.com>
2021-06-18 10:38:11 -0300

    add function that generates cards from client list

--- lib/views/screens/list_cards_screen.dart
+++ lib/views/screens/list_cards_screen.dart
@@ -1,19 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:formulario/controllers/dao/client_dao.dart';
+import 'package:formulario/models/client.dart';
+import 'package:formulario/views/screens/loading_cards_screen.dart';
+import 'package:formulario/views/widgets/card_cliente.dart';
 
 class ListCardsPage extends StatefulWidget {
-  final List<Widget> containers;
+  final List<Client> clients;
 
-  ListCardsPage({required this.containers});
+  ListCardsPage({required this.clients});
 
   @override
-  _ListCardsPageState createState() =>
-      _ListCardsPageState(containers: containers);
+  _ListCardsPageState createState() => _ListCardsPageState(clients: clients);
 }
 
 class _ListCardsPageState extends State<ListCardsPage> {
-  List<Widget> containers;
+  List<Client> clients;
+  ClientDAO _dao = ClientDAO();
 
-  _ListCardsPageState({required this.containers});
+  _ListCardsPageState({required this.clients});
+
+  List<Widget> _getContainers() {
+    List<Widget> containers = [];
+    if (clients.isNotEmpty) {
+      clients.forEach((element) {
+        containers.add(CardCliente(
+          client: element,
+          onEditing: () => print(element.nome),
+          onRemove: () async {
+            String res =
+                await _dao.delete(where: 'nome', whereArgs: element.nome);
+            if (res == 'success') {
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text('Usuario excluido.')));
+
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => LoadingCardsPage(),
+                ),
+              );
+            }
+          },
+        ));
+      });
+    }
+    return containers;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +56,7 @@ class _ListCardsPageState extends State<ListCardsPage> {
       body: SingleChildScrollView(
         child: SafeArea(
             child: Column(
-          children: containers,
+          children: _getContainers(),
         )),
       ),
     );
