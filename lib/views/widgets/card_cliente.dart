2d6d8c42dedd904523e4fc4dc5c7e72ab4ec028f
lps08 <lucianolps08@gmail.com>
2021-06-21 11:06:23 -0300

    add onPress function

--- lib/views/widgets/card_cliente.dart
+++ lib/views/widgets/card_cliente.dart
@@ -3,56 +3,67 @@ import 'package:formulario/models/client.dart';
 
 class CardCliente extends StatelessWidget {
   final Client client;
-  final void Function() onEditing;
-  final void Function() onRemove;
+  final void Function()? onEditing;
+  final void Function()? onRemove;
+  final void Function() onPress;
 
   CardCliente(
-      {required this.client, required this.onEditing, required this.onRemove});
+      {required this.client,
+      required this.onPress,
+      this.onEditing,
+      this.onRemove});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        elevation: 5.0,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  child: FittedBox(
-                    child: Icon(Icons.account_box),
+      child: InkWell(
+        onTap: onPress,
+        child: Card(
+          elevation: 5.0,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    child: FittedBox(
+                      child: Icon(Icons.account_box),
+                    ),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextCardTitle(title: 'Nome: ', name: client.nome),
-                    TextCardTitle(
-                        title: 'Nascimento: ',
-                        name: client.nascimento.split(' ')[0])
-                  ],
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: onEditing,
-                  child: Text('Editar'),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                TextButton(
-                  onPressed: onRemove,
-                  child: Text('Excluir'),
-                ),
-              ],
-            )
-          ],
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextCardTitle(title: 'Nome: ', name: client.nome),
+                      TextCardTitle(
+                          title: 'Nascimento: ',
+                          name: client.nascimento.split(' ')[0])
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  onEditing != null
+                      ? TextButton(
+                          onPressed: onEditing,
+                          child: Text('Editar'),
+                        )
+                      : Text(''),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  onRemove != null
+                      ? TextButton(
+                          onPressed: onRemove,
+                          child: Text('Excluir'),
+                        )
+                      : Text(''),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
